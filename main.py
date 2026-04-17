bd0cef422d4d86a69a700d66c7fc1b7afd802c1c
Jary Tolentino <jt3577@columbia.edu>
2026-04-16 20:24:00 -0400

    added parse order function

--- main.py
+++ main.py
@@ -30,24 +30,26 @@ class RequestFilters(BaseModel):
     buyer: Optional[str] = None
     items: Optional[List[str]] = None
 
-request = input("What would you like to do? ")
-
 class AgentState(BaseModel):
     # Raw request data
     user_request: str
+    raw_orders: List[str]
     filters: RequestFilters
-    orders: List[Order]
+    parsed_orders: List[Order]
 
-def read_request(request) -> dict:
+request = input("What would you like to do? ")
+
+def read_request(state: AgentState) -> dict:
     """Extract and parse request content"""
 
+    state['user_request'] = request
     return {
-        "messages": [HumanMessage(content=f"Processing request: {request}")]
+        "messages": [HumanMessage(content=f"Processing request: {state['user_request']}")]
     }
 
 def get_request_filters(state: AgentState):
     """Use the LLM to get request filters"""
-    structured_llm = llm.with_structured_output(Order)
+    structured_llm = llm.with_structured_output(RequestFilters)
 
     prompt = f"""
     Analyze this customer request and get the filters being passed:
@@ -55,8 +57,35 @@ def get_request_filters(state: AgentState):
     Request: {state['user_request']}
     
     Provide optional filters passed including min_total, max_total, location,
-    total_price, and items.
+    buyer, and items.
     """
 
     filters = structured_llm.invoke(prompt)
+
+    # add error check and validation here
+
+    return Command(
+        update={"filters": filters},
+        goto="parse_orders"
+    )
+
+def parse_orders(state: AgentState):
+    """Use the LLM to structure orders"""
+    structured_llm = llm.with_structured_output(Order)
+    parsed_orders = []
+    for raw_order in state['raw_orders']:
+        prompt = f"""
+        Analyze this order.
+
+        Order: {raw_order}
+        
+        Provide the order_num, buyer, location, total_price,
+        and the list of items.
+        """
+        parsed_orders.append(structured_llm.invoke(prompt))
+    
+    return Command(
+        update={"parsed_orders": parsed_orders},
+        goto="filter_orders"
+    )
 
