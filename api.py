39b411da160b62502ed45df4fc28cbc8ca26b360
Jary Tolentino <jt3577@columbia.edu>
2026-04-20 13:09:51 -0400

    added request endpoint

--- api.py
+++ api.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from main import AgentState, agent
 
 app = FastAPI()
 
+origins = []
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Request(BaseModel):
+    request: str
+
+@app.post("/request/")
+async def create_request(request: Request):
+    initial_state = AgentState(user_request = request)
+    result = agent.invoke(initial_state)
+
+    request_type = result["parsed_request_type"].request_type
+    
+    if request_type == "order":
+        filtered_orders = result["filtered_orders"]
+        if not filtered_orders:
+            return {"Error": "Order not found."}
+        else:
+            output = {"orders": [order.model_dump(exclude={'tech_count, accessory_count, audio_count, homegoods_count'}) for order in filtered_orders]}
+            return output
+    elif request_type == "prediction":
+        prediction_result = result["prediction_result"]
+        if not prediction_result:
+            return {"Error": "Prediction failed."}
+        else:
+            return {"predicted_total": round(float(prediction_result[0]), 2)}
+    else:
+        return {"Error": "Invalid Request!"}
+    
